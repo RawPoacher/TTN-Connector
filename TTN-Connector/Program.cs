d626065ba6278fe0c1269d6093d023e35892bd57
Rene Schreurs <rene@rpia.nl>
2016-07-02 23:49:40 +0200

    * Refactored initial code * Will retry to connect if connection is lost or unable to connect * Changed topic to +/+/+/up * Use persistent session

--- TTN-Connector/Program.cs
+++ TTN-Connector/Program.cs
@@ -10,53 +10,88 @@ using uPLibrary.Networking.M2Mqtt.Internal;
 using uPLibrary.Networking.M2Mqtt.Session;
 using uPLibrary.Networking.M2Mqtt.Utility;
 
+
+// TODO: use TLS
+
 namespace TTN_Connector
 {
     class Program
     {
+
+        private const string AppEUI = @"70B3D57ED0000538"; // TODO: make configurable
+        private const string AccessKey = @"Dj8ebO/p+agcxgKoRMmF0rKd07X7m4idj+M6HiS48EY="; // TODO: make configurable
+        private const string mqttBrokerHostName = "rene-ttn-test"; // TODO: make configurable
+        private const string clientId = "RSTest";
+        private const string topicFilter = @"+/+/+/up";
+        private static MqttClient client = null;
+        private const int retryDelay = 1000;
+
         static void Main(string[] args)
         {
-            string AppEUI = @"70B3D57ED0000538"; // TODO: make configurable
-            string AccessKey = @"Dj8ebO/p+agcxgKoRMmF0rKd07X7m4idj+M6HiS48EY="; // TODO: make configurable
-
-            string username = AppEUI;   
-            string password = AccessKey;   
-
-            string mqttBrokerHostName = "rene-ttn-test"; // TODO: make configurable
-                                                         // TODO: use TLS
-
-            Console.WriteLine("starting up...");
-            MqttClient client = new MqttClient(mqttBrokerHostName); // TODO: add error handling!
-
+            client = new MqttClient(mqttBrokerHostName); // TODO: add error handling!
+            
             // Register event handler for Connection being closed
             client.ConnectionClosed += client_ConnectionClosed;
-
-            // register event handler for received messages
-            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
             // Register event handler for subscription
             client.MqttMsgSubscribed += client_MqttMsgSubscribed;
 
-            string clientId = "RSTest";
-            client.Connect(clientId, username, password);   // TODO: add error handling
+            // register event handler for received messages
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
-            // subscribe to the topic ( all Applications, all Devices
-            client.Subscribe(new string[] { @"+/devices/+/up" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            Connect();
 
+            // subscribe to the topic ( all Applications, all Devices)
+            // TODO: check if QOS_LEVEL is the right one for this application; QOS Level 1 should be OK
+            // TODO: error handling
+            client.Subscribe(new string[] { topicFilter }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            DoStuff();
+
+            Disconnect();
+        }
+
+        static void DoStuff()
+        {
             Console.WriteLine("Press <Enter> to quit");
             Console.ReadLine();
+        }
 
-            // Remove event handler for Connection being closed
-            client.ConnectionClosed -= client_ConnectionClosed;
-            client.Disconnect();
+        static async void Connect()
+        {
+            // Make a connection with a persistent session. Send keep-alive every 60 seconds 
+            _connect();
 
-            Console.WriteLine("Ended!");
+            while (!client.IsConnected)
+            {
+                await DelayTask(retryDelay);
+                _connect();
+            }
+            Console.WriteLine("Connected");
+        }
+
+        static void _connect()
+        {
+            Console.WriteLine("Connecting...");
+            client.Connect(clientId, AppEUI, AccessKey, false, 60);
+        }
+
+        static void Disconnect()
+        {
+            if (client.IsConnected)
+            {
+                // Remove event handler for Connection being closed
+                client.ConnectionClosed -= client_ConnectionClosed;
+                client.Disconnect();
+            }
         }
 
         static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
+
+            // TODO: handle message received (instead of output to console)
+            Console.WriteLine("Topic: "+ e.Topic);
             var str = System.Text.Encoding.Default.GetString(e.Message);
-            // handle message received 
             Console.WriteLine(str);
         }
 
@@ -65,9 +100,16 @@ namespace TTN_Connector
             Console.WriteLine("Subscribed for id = " + e.MessageId);
         }
 
-         static void client_ConnectionClosed(object sender, EventArgs e)
+        static void client_ConnectionClosed(object sender, EventArgs e)
         {
             Console.WriteLine("Connection lost.");
+            Connect();
         }
+
+        static async Task DelayTask(int milisecondsDelay)
+        {
+            await Task.Delay(milisecondsDelay);
+        }
+
     }
 }
